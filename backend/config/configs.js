99d30a9a762b4d0bca0a6fcac992ed0b5c69f79a
Monali Kumari <you@monali151998@gmail.com>
2023-05-15 20:50:44 +0530

    first commit

---
+++ backend/config/configs.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database: 'mongodb+srv://monali151998:Rabbit120@singh.gm7mtqw.mongodb.net/taskmanager',
+    secret: '$ecreate12345',
+    enableConsoleLog: true,
+    enableFileLog:false,
+    tokenExpire: 36000 //10 hours
+}
